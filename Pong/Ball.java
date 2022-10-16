0143e3e863b34dbc2eb42df5c8dd91e4ce9a081f
FelixLin6 <68569236+FelixLin6@users.noreply.github.com>
2022-10-16 00:50:02 -0700

    updates

--- Pong/Ball.java
+++ Pong/Ball.java
@@ -1,38 +1,35 @@
 package Pong;
 import java.awt.Rectangle;
-import java.util.ArrayList;
 
 public class Ball extends Rectangle{
-	private double angle = 45.0;
-    private double dr = 10.0;
-    private int width;
-    private double dx = this.dr*Math.cos(this.angle);
-	private double dy = this.dr*Math.sin(this.angle);
-	
-	private double leftEdge = this.getCenterX()-width/2;
-	private double rightEdge = this.getCenterX()+width/2;
-	private double upperEdge = this.getCenterY()+width/2;
-	private double lowerEdge = this.getCenterY()-width/2;
+	private int radius;
+    private double dx = 20;
+	private double dy = -20;
+
+	private double leftEdge = this.getCenterX()-radius;
+	private double rightEdge = this.getCenterX()+radius;
+	private double upperEdge = this.getCenterY()-radius;
+	private double lowerEdge = this.getCenterY()+radius;
 	
     public Ball(int radius) {
         super(200, 300, radius*2, radius*2);
-        this.width = radius*2;
+        this.radius = radius;
     }
     
     public void checkDir() {
-    	if((int)upperEdge<=0 || (int)lowerEdge>=PongRunner.getMaxWindowY()) {
-    		if(dx>0&&dy>0) {this.angle = 360-(90-angle);}
-    		else if (dx<0&&dy<0) {this.angle = angle-90;}
-    		else {this.angle = 360%(angle+90);}
-    	}
+    	if((int)upperEdge<=199 || (int)lowerEdge>=PongRunner.getMaxWindowY()) {
+			dx *= -1;
+			dy *= -1;
+		}
     }
     
-    public void update() {
-
-    	ArrayList<java.lang.Double> components = PongRunner.dir(this.angle, x, y);
-    	this.dx = components.get(0);
-    	this.dy = components.get(1);
+    public void update(){
     	this.checkDir();
-    	this.move((int)(this.getX()+dx), (int)(this.getY()+dy));
+		try {
+			Thread.sleep(100);
+		} catch(InterruptedException e) {
+			System.out.println("got interrupted!");
+		}
+    	this.setLocation((int)(this.getX()+dx), (int)(this.getY()+dy));
     }
 }
--- Pong/PongRunner.java
+++ Pong/PongRunner.java
@@ -2,46 +2,10 @@ package Pong;
 import utilities.GDV5;
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
 
 
 public class PongRunner extends GDV5{
-	
-	public static ArrayList<Double> dir(double theta, double x, double y) {
-		ArrayList<Double> xycomponents = new ArrayList<Double>();
-		if(90<theta && theta<180){
-			xycomponents.add(-x); 
-			xycomponents.add(y);
-			
-		} 
-		else if(0<theta && theta<90) {
-			xycomponents.add(x);
-			xycomponents.add(y);
-		}
-		else if(180<theta && theta<270) {
-			xycomponents.add(-x);
-			xycomponents.add(-y);
-		}
-		else if(270<theta && theta<360) {
-			xycomponents.add(x);
-			xycomponents.add(-y);
-		}
-		else if(theta==0 || theta==360 || theta==90) {
-			xycomponents.add(x);
-			xycomponents.add(y);
-		}
-		else if(theta==270) {
-			xycomponents.add(-x);
-			xycomponents.add(y);
-		}
-		else {
-			xycomponents.add(x);
-			xycomponents.add(-y);
-		}
-		return xycomponents;
-	}
-	
+
     Ball ball = new Ball(12);
     
     public static void main(String[] args) throws InterruptedException {
